baf0e9f6602776091f91a43b54e82f81cd5acb9b
Sebcoach13 <140277344+Sebcoach13@users.noreply.github.com>
2026-05-21 23:36:30 +0200

    mise a jour commentaires

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from models import create_member
 
+# création de l'application Flask
 app = Flask(__name__)
+
 list_of_members = []
 
-# 1. Cette route affiche le formulaire quand on va sur le site
+# 1. affichage du formulaire quand on va sur le site
 @app.route('/')
 def home():
     return render_template('formulaire.html')
@@ -12,18 +14,18 @@ def home():
 # 2. Cette route reçoit les données du formulaire quand on clique sur le bouton
 @app.route('/inscription', methods=['POST'])
 def inscription():
-    # On récupère ce que l'utilisateur a tapé dans les cases HTML
+    # récuperation du texte tapé dans les cases HTML
     nom = request.form.get('nom')
     prenom = request.form.get('prenom')
     age = int(request.form.get('age'))
     taille = float(request.form.get('taille'))
     
-    # On utilise ton moule dans models.py !
+    # utilisation du modèle dans models.py !
     membre = create_member(nom, prenom, age, taille)
     list_of_members.append(membre)
     
-    # On affiche la liste des membres en format JSON (texte propre) à l'écran
-    return jsonify({"message": "Membre inscrit avec succès !", "liste_actuelle": list_of_members})
+    # affichage de la page  succès HTML !
+    return render_template('succes.html', nom=nom, prenom=prenom)
 
 if __name__ == "__main__":
     # Lance le serveur web local
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,18 @@
 import sys
 import os
 
-# On aide Python à trouver le dossier 'app' depuis le dossier 'tests'
+# Trouver le dossier 'app' depuis le dossier 'tests'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
 
 from models import create_member
 
 def test_creation_membre():
-    # On simule la création
+    # simuler la création
     resultat = create_member("DA COSTA", "Sébastien", 53, 1.80)
     
-    # On vérifie que c'est bien un dictionnaire
+    # vérification dictionnaire
     assert type(resultat) is dict
-    # On vérifie que le nom est correct
+    # vérification du nom
     assert resultat["nom"] == "DA COSTA"
     
     print("✅ TEST REUSSI : Le moule 'models.py' fabrique des membres corrects.")
